7f1ee5391f504ca318cfbd4cb3d6be07d320b6a7
BriagasD98 <briagasdavid@yahoo.com>
2021-09-12 00:43:25 -0500

    Thought model created, reaction schema created

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,28 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    })
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -18,7 +40,7 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true
         },
-        reactions: [reactionSchema]
+        reactions: [ReactionSchema]
     },
         {
             toJson: {
