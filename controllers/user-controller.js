fa1c284cc5f974ce62a70d9d3821522167fcc34e
BriagasD98 <briagasdavid@yahoo.com>
2021-09-13 21:22:52 -0500

    comment update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -88,7 +88,7 @@ const userController = {
         .catch(err => res.status(400).json(err));
     },
 
-    // REMOVE reaction
+    // REMOVE friend
     removeFriend({ params }, res) {
         User.findOneAndUpdate(
             { _id: params.id },
